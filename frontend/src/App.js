906093ec7f6ddcd7ada405be20d2c147e85136d9
Gitali03 <aonermtk@gmail.com>
2026-02-05 14:11:32 +0300

    added RootLayout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,18 +25,24 @@ import HomePage from "./pages/Home";
 import EventsPage from "./pages/Events";
 import EventDetailPage from "./pages/EventDetail";
 import NewEventPage from "./pages/NewEvent";
+import EditEventPage from "./pages/EditEvent";
+import RootLayout from "./pages/Root";
 const router = createBrowserRouter([
-  { path: '/', element: <HomePage/>},
-  { path: '/events', element: <EventsPage/>},
-  { path: '/events/:eventId', element: <EventDetailPage/>},
-  { path: '/events/new', element:<NewEventPage/>},
-  { path: '/events/:eventId/edit', element: <EditEventPage/>}
-
-
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "events", element: <EventsPage /> },
+      { path: "events/:eventId", element: <EventDetailPage /> },
+      { path: "/events/new", element: <NewEventPage /> },
+      { path: "/events/:eventId/edit", element: <EditEventPage /> },
+    ],
+  },
 ]);
 
 function App() {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
 
 export default App;
