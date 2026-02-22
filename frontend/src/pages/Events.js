fa4d81c3b9ee7326bc0e3a3676bced00eb2c43ed
Gitali03 <aonermtk@gmail.com>
2026-02-22 13:28:50 +0300

    Extracting Error Data and throwing responses

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,18 +1,34 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, json } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 export default function EventsPage() {
-  const events = useLoaderData();
+  const data = useLoaderData();
+
+  //if (data.isError) {
+  // return <>{data.message}</>
+  //}
+  const events = data.events;
   return <EventsList events={events} />;
 }
 
 export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
+
   if (!response.ok) {
-    //..
-  } else {
-    const resData = await response.json();
-    return resData.events;
-  }
+    //return { isError: true, message: 'Could not fetch events'};
+    //throw new Response(JSON.stringify({ message: "Could not fetch events" }), {
+    //status: 500,
+    throw json(
+      { message: "Could not fetch events" },
+      {
+        status: 500,
+      },
+    );
+  } 
+  const resData = await response.json();
+
+  return {
+    events: resData.events,
+  };
 }
