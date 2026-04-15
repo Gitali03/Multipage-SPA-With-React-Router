d5cde2203345988a685cac2eb5a0d0e4c2113a06
Gitali03 <aonermtk@gmail.com>
2026-04-15 20:57:51 +0300

    added authentication form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -34,6 +34,7 @@ import EventsRootLayout from "./pages/EventsRoot";
 import ErrorPage from "./pages/Error";
 import { action as manipulateEventAction } from "./components/EventForm";
 import NewsletterPage, { action as newsletterAction} from './pages/Newsletter';
+import AuthenticationPage from "./pages/Authentication";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -41,6 +42,13 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
+      {
+        
+      },
+      {
+        path:'/auth',
+        element: <AuthenticationPage/>
+      },
       {
         path: "events",
         element: <EventsRootLayout />,
--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -38,9 +38,19 @@ function MainNavigation() {
               Newsletter
             </NavLink>
           </li>
+          <li>
+            <NavLink
+              to="/auth"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Authentication
+            </NavLink>
+          </li>
         </ul>
       </nav>
-      <NewsletterSignup/>
+      <NewsletterSignup />
     </header>
   );
 }
